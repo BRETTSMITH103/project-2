1d431a91559e5bc29cd54d0969d68b3713065a06
chenallee <lchenalloy@gmail.com>
2020-03-12 19:01:42 -0400

    setting up files with connections

---
+++ middleware/check-auth.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+
+const checkAuth = (req, res, next) => {
+  let token =
+    req.body.token ||
+    req.query.token || 
+    req.query.token ||
+    req.headers['x-access-token'] ||
+    req.headers.authorization;
+
+    // check if token is authorization token
+    /*
+    headers: {
+      Authorization: 'Bearer <token>'    
+    } 
+    */
+
+    if (req.headers.authorization){
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
+    }
+
+    //check if jw token is valid
+    jwt.verify(token, 'R4BLW4TCH', (err, decoded) => { //should the secret key be in the .env file?
+      if(err) {
+        res.status(400).json({
+          success: false,
+          message: 'Unauthorized: invalid token'
+        });
+        return;
+      }
+      req.email = decoded.email;
+      req.id = decoded.id;
+      next();
+    }); 
+};
+
+module.exports = checkAuth;
