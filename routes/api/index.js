27dfa9904b711234ba4dc2341a5b40d150fdfffe
Lee Chenalloy <lilly.chenalloy@gmail.com>
2020-03-14 11:32:19 -0400

    Update index.js
    word post changed to word show

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,12 +2,12 @@
 const router = require('express').Router();
 
 // import api route files
-const postRoutes = require('./post-routes');
+const postRoutes = require('./show-routes');
 const userRoutes = require('./user-routes');
 const authRoutes = require('./auth-routes');
 
 // set up and prepend api routes from imported files
-router.use('/posts', postRoutes);
+router.use('/posts', showRoutes);
 router.use('/users', userRoutes);
 router.use('/auth', authRoutes);
 
