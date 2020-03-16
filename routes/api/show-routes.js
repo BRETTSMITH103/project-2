4a846561cb1145e4b89cd94377b5b66db11b5122
chenallee <lchenalloy@gmail.com>
2020-03-16 00:40:12 -0400

    all routrs working with auth

--- routes/api/show-routes.js
+++ routes/api/show-routes.js
@@ -5,16 +5,16 @@ const { Show, User } = require('../../models');
 
 // import auth middleware
 const checkAuth = require('../../middleware/check-auth');
+const jwt = require('jsonwebtoken');
 
 const Sequelize = require('sequelize');
 
 const Op = Sequelize.Op;
 
-// get all Shows with associated users
-
+// get all Shows - we might not need this for users but we might need it for our queries
 // https://gist.github.com/zcaceres/83b554ee08726a734088d90d455bc566
 
-router.get('/', checkAuth, (req, res) => {
+router.get('/all', (req, res) => {
   Show.findAll()
     .then(showdata => res.json(showdata))
     .catch(err => {
@@ -23,20 +23,56 @@ router.get('/', checkAuth, (req, res) => {
     });
 });
 
-// get shows by id with associated User
-router.get('/:query', (req, res) => {
+// get all of the shows associated with the authenticated user ('on my watchlist')
+// we also want this to be the default
+router.get('/me', checkAuth, (req, res) => { //how do we do or? we want this route to be '/' OR '/me'
+
+  const token = (req.headers.authorization)
+    .split(' ')
+    .pop()
+    .trim();
+  const payload = jwt.decode(token);
+
+  Show.findAll({
+    where: {
+      UserId: payload.data.id
+    },
+  })
+    .then(showdata => res.json(showdata))
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+
+// get show by id or title associated with authenticated User ('on my watchlist')
+router.get('/me/:query', checkAuth, (req, res) => {
+
+  const token = (req.headers.authorization)
+    .split(' ')
+    .pop()
+    .trim();
+  const payload = jwt.decode(token);
+
   Show.findOne({
     where: {
-      [Op.or]:[
+      [Op.and]: [
         {
-      id: req.params.query
+          UserId: payload.data.id
         },
         {
-      email: req.params.query
+          [Op.or]: [
+            {
+              id: req.params.query
+            },
+            {
+              title: req.params.query
+            }
+          ]
         }
-      ]    
+      ]
     },
-    include: [User]
   })
     .then(showdata => res.json(showdata))
     .catch(err => {
@@ -47,23 +83,27 @@ router.get('/:query', (req, res) => {
 
 // create a new show entry
 
-router.post('/', checkAuth, (req, res) => {
+router.post('/me/', checkAuth, (req, res) => {
   // /* 
-  
+  //{
   // title: "Show Title",
   // toWatch: true,
   // watching: false,
   // completed: false,
   // UserId: 1
 
-//OPTIONAL? Depending on how we wanna do this:
+  //OPTIONAL? Depending on how we wanna do this:
   // TvMazeId: 32
-// }
+  // }  
 
-  
+  const token = (req.headers.authorization)
+    .split(' ')
+    .pop()
+    .trim();
+  const payload = jwt.decode(token);
 
   // set UserID
-  req.body.UserId = req.id;
+  req.body.UserId = payload.data.id;
 
   Show.create(req.body)
     .then(showdata => res.json(showdata))
@@ -73,20 +113,36 @@ router.post('/', checkAuth, (req, res) => {
     });
 });
 
-// update shows by id or email
-router.put('/:query', (req, res) => {
+// update shows by id or title
+router.put('/me/:query', checkAuth, (req, res) => {
+  const token = (req.headers.authorization)
+    .split(' ')
+    .pop()
+    .trim();
+  const payload = jwt.decode(token);
+
+  // set UserID
+  req.body.UserId = payload.data.id;
+
+
   Show.update(req.body, {
     where: {
-      [Op.or]: [
+      [Op.and]: [
         {
-          id: req.params.query
+          UserId: payload.data.id
         },
         {
-          email: req.params.query
+          [Op.or]: [
+            {
+              id: req.params.query
+            },
+            {
+              title: req.params.query
+            }
+          ]
         }
       ]
     },
-    include: [User]
   })
     .then(showdata => res.json(showdata))
     .catch(err => {
@@ -94,9 +150,9 @@ router.put('/:query', (req, res) => {
       res.json(err);
     });
 });
- 
+
 // delete show by id, do we want to delete shows?
-// router.delete('/:id', (req, res) => {
+// router.delete('/me/:query', (req, res) => {
 //   User.destroy({
 //     where: {
 //       id: req.params.query
